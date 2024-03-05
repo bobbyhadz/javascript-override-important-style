f1db5c7deedd98f7bbc0a96ecddeab44b8ff19e3
bobbyhadz <opmftw@gmail.com>
2024-03-05 13:29:02 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('bobbyhadz.com');
+
+const box = document.getElementById('box');
+
+box.style.setProperty('display', 'block', 'important');
+box.style.setProperty('background-color', 'lime', 'important');
